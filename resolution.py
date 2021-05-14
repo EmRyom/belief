75f538b9ff297a1fe59a07ab052f805e3a35b43d
Nikita <roshkoff1@gmail.com>
2021-05-14 12:50:51 +0200

    resolution final

--- resolution.py
+++ resolution.py
@@ -8,9 +8,7 @@ def addDisjunBack(finSet):
         finString += str(el) + "|" 
     finString = finString[:-1]
     evalStr = eval(finString)
-    #print("evalStr ", evalStr)
     result = set([evalStr])
-     
     return result
 
 
@@ -35,21 +33,13 @@ def splitClause(x_):
 #Receives two clauses, unites them and gets rid of complementary literals
 #Returns clause
 def resolve(x_, y_):
-    #print("x= ", x_,"    y= ", y_)
     x = splitClause(x_)
     y = set(y_.copy())
     finSet = set([]) 
     
-    for n in range(len(x)):
-              
+    for n in range(len(x)):      
         element = x.pop()
         ss = set([~element])
-        
-        
-        
-        #print("subset: ", ss.issubset(splitClause(y) ))
-        
-
         if ss.issubset(splitClause(y)):
             ssTemp = ss.pop()
             yyy = splitClause(y)
@@ -62,7 +52,6 @@ def resolve(x_, y_):
     if len(finSet) > 0: 
         finSet = addDisjunBack(finSet)  
     finSet.update(y)
-    #print ("finset: ", finSet , "\n")
     return finSet
 
 
@@ -72,90 +61,62 @@ def resolve(x_, y_):
 def resolution (kb_,alpha_):  
     kb = set(kb_.copy())
     alpha = set(alpha_.copy())
-    
-    #same as kb | alpha
     init_clauses = kb.union(alpha)
-    cons = False
     finSet = set([])
+    prevFinSet= set([])
     
     
-    N_clausesImmutable = init_clauses.copy()
-    f1, f2 = set([]), set([])
+    while True:
+        N_clausesImmutable = init_clauses.copy()
+        f1, f2 = set([]), set([])
+         
+        for n in N_clausesImmutable:
+            f1.add(n)
+            init_clauses.discard(n)
+            M_clausesImmutable = init_clauses.copy()
+            
+            for m in M_clausesImmutable:    
+                f2.add(m)
+                ca = resolve(f1, f2)    
+                finSet = finSet.union(ca.copy())
     
+                if len(ca) == 0: 
+                    return True
     
-   
-    for n in N_clausesImmutable:
-        f1.add(n)
-        init_clauses.discard(n)
-        M_clausesImmutable = init_clauses.copy()
+                if f2:
+                    f2.pop()
+                  
+                
+            if f1:
+                f1.pop()
+     
+            
+        #if could did not found empty clause that was yield, and the final Set
+        #is same as in prev iteration
+        if len(prevFinSet)>0 and prevFinSet==finSet:
+            return False
         
-        for m in M_clausesImmutable:
-            f2.add(m)
-            print("f1==", f1, "vs: f2==", f2)
-            ca = resolve(f1, f2)    
-            
-            print(" ca : ", ca)
-            #finSet.discard({b})
-            #finSet.discard(f2)
-          
-            finSet = finSet.union(ca.copy())
-            
-            #INSTEAD OF DELETING FROM FINSET, WE ACTUALLY NEED TO DELETE FROM initial_clauises
-            f1safe = f1.pop()
-            finSet.discard(f1safe)
-            f1.update([f1safe])
-            
-            f2safe = f2.pop()
-            finSet.discard(f2safe)
-            f2.update([f2safe])
-            
-            print("    finSet: ", finSet ,"\n")
-            
-           
-            if len(ca) == 0: 
-                return True
-                #finSet.remove(f1.pop())
-                #finSet.remove(f2.pop())
-            
+        finSet = finSet.union(init_clauses)
+        init_clauses = init_clauses.union(finSet)
+        prevFinSet = finSet
 
-            if f2:
-                f2.pop()
-              
-            
-        if f1:
-            f1.pop()
 
-        
-    finSet = finSet.union(init_clauses)
-    print ("== finSet", finSet) 
-    
-    #print("finset: ", finSet, "   vs   init_clauses: ", init_clauses)
-    #if there are no new clauses in final kb compared to initial kb,
-    #then kb does not entail alpha
-    if finSet.issubset(init_clauses):
-        return False
-            
-        
-        #return cons
    
             
-                
-                
-    
- 
-#result = resolve([c, b, a, d, ~g], [~a, ~c, g])
-#print("result is ", result)
 
-kbb = set([a|~c, b])
-#kb = set([~a|b, ~c|b, ~b])
-alpha = set([c])
-print("Resoluition: ", resolution (kbb,alpha))
+####
+#kbE = set([~a|b|c,~b|a,~c|a,~a])
+#alphaE = set([b])
 
-t = set([a|~c])
-#resolve(t,alpha)
+kbF = set([~a|b, c])
+kbT = set([~a|b, ~c|b, c])
+alpha = set([~b])
+resol = resolution (kbT,alphaT)
 
-'''
-yyx = splitClause(t)
-print("yyx ",yyx)
-print("alpha is subset of y?", alpha.issubset(yyx))
-'''
+print("Resolution: \n");
+if resol:
+    print("True. KB entails alpha")
+else:
+    print("False. KB does not entail alpha")
+
+
